4af4582ee8acf411959bc8efb63148dbbcae0eaa
dragisa-zaric <dragisaza@gmail.com>
2017-04-29 15:13:01 +0200

    Filler panels
    Construction panels and fill with JToggleButtons dimensions 9x9

--- Minesweeper/src/minesweeper/Minesweeper.java
+++ Minesweeper/src/minesweeper/Minesweeper.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package minesweeper;
 
-/**
- *
- * @author user
- */
+
 public class Minesweeper {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+      Sweep sw = new Sweep();
+      sw.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
+      sw.show();
     }
     
 }
---
+++ Minesweeper/src/minesweeper/Sweep.java
@@ -0,0 +1,72 @@
+
+package minesweeper;
+import javax.swing.JToggleButton;
+public class Sweep extends javax.swing.JFrame {
+    /*
+    -1: has a bomb
+    0: not open
+    1 - 8: number of bombs
+    */
+    final int wid = 9, hei = 9, noOfBombs = 10;
+    JToggleButton[][] blocks = new JToggleButton[hei][wid];
+    int[][] blox = new int[hei][wid];
+    
+    public Sweep() {
+        initComponents();
+        for(int i = 1; i < hei; i++){
+            for(int j = 1; j < wid; j++){
+                blocks[i][j] = new JToggleButton();
+                blocks[i][j].setSize(jPanel1.getWidth()/wid, jPanel1.getHeight()/hei);
+                jPanel1.add(blocks[i][j]);
+                blocks[i][j].setLocation(j*jPanel1.getWidth()/wid, i*jPanel1.getHeight()/hei);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 278, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Sweep().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
