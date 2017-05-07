8e73a694ba65648f046a40e65c29f8151ed50d6a
dragisa-zaric <dragisaza@gmail.com>
2017-05-07 23:29:29 +0200

    Resize options
    Resize options and actionListener

--- Minesweeper/src/minesweeper/Sweep.java
+++ Minesweeper/src/minesweeper/Sweep.java
@@ -1,6 +1,8 @@
 
 package minesweeper;
 import javax.swing.JToggleButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class Sweep extends javax.swing.JFrame {
     /*
     -1: has a bomb
@@ -10,19 +12,48 @@ public class Sweep extends javax.swing.JFrame {
     final int wid = 9, hei = 9, noOfBombs = 10;
     JToggleButton[][] blocks = new JToggleButton[hei][wid];
     int[][] blox = new int[hei][wid];
+    ActionListener listen = new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            int i = 0, j = 0;
+            boolean found = false;
+            for(i = 0; i < hei; i++){
+                for(j = 0; j < wid; j++){
+                    if(e.getSource() == blocks[i][j]){
+                        found = true;
+                        break;
+                    }
+                }
+                if(found) break;
+            }
+            blocks[i][j].setSelected(true);
+        }
+    };
+    
+    
     
     public Sweep() {
         initComponents();
-        for(int i = 1; i < hei; i++){
-            for(int j = 1; j < wid; j++){
+        for(int i = 0; i < hei; i++){
+            for(int j = 0; j < wid; j++){
                 blocks[i][j] = new JToggleButton();
+                blocks[i][j].setSize(jPanel1.getWidth()/wid, jPanel1.getHeight()/hei);
+                jPanel1.add(blocks[i][j]);
+                blocks[i][j].setLocation(j*jPanel1.getWidth()/wid, i*jPanel1.getHeight()/hei);
+                blocks[i][j].addActionListener(listen);
+            }
+        }
+    }
+
+    private void resize(){
+        for(int i = 0; i < hei; i++){
+            for(int j = 0; j < wid; j++){
                 blocks[i][j].setSize(jPanel1.getWidth()/wid, jPanel1.getHeight()/hei);
                 jPanel1.add(blocks[i][j]);
                 blocks[i][j].setLocation(j*jPanel1.getWidth()/wid, i*jPanel1.getHeight()/hei);
             }
         }
     }
-
+    
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -30,11 +61,17 @@ public class Sweep extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentResized(java.awt.event.ComponentEvent evt) {
+                jPanel1ComponentResized(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 380, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -45,11 +82,14 @@ public class Sweep extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
@@ -57,6 +97,10 @@ public class Sweep extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jPanel1ComponentResized(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jPanel1ComponentResized
+        resize();
+    }//GEN-LAST:event_jPanel1ComponentResized
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
